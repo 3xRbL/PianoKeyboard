c6c3be02d8589f03d5745e5ab9dfee9cf2c38f0c
Silvestras Rukša <forfreecsgo@gmail.com>
2019-06-21 14:21:01 +0300

    Started adding basic source code

---
+++ src/MainScene.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+#Volume Slider
+#Max X = 508
+#Min X = 50
+var move_slider = false
+
+func _ready():
+	pass # Replace with function body.
+
+#func _process(delta):
+#	pass
+
+func _input(event):
+	pass
